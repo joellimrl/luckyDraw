44d7e30d02b97b5cf4cc1b0191caf06ca37dfe0d
Joel Lim <joellimruili@gmail.com>
2024-09-09 19:00:01 +0800

    init changes

--- common.js
+++ common.js
@@ -1,145 +1,127 @@
 let slideIndex = 0;
 
-const totalSlides = 29;
+const totalSlides = 23;
 const allImages = [
-    "image/1.png", //29
-    "image/1.png", //28
-    "image/26.png", //27 
-    "image/26.png", //26 
-    "image/25.png", //25
-    "image/1.png", //23
-    "image/1.png", //22
-    "image/1.png", //21
-    "image/1.png", //20
-    "image/19.png", //19
-    "image/2.png", //18
-    "image/17.png", //17 
-    "image/3.png", //16
-    "image/15.png", //15 
-    "image/14.png", //14
-    "image/14.png", //13
-    "image/4.png", //12
-    "image/27.png", //12 jo malone candle
-    "image/5.png", //11
-    "image/6.png", //10
-    "image/16.png", //9
-    "image/20.png", //8
-    "image/9.png", //7
-    "image/7.png", //6
-    "image/8.png", //5
-    "image/12.png", //4
-    "image/11.png", //3
-    "image/10.png", //2
-    "image/13.png"  //1
+    "image/takashimaya.png", //23
+    "image/takashimaya.png", //22
+    "image/takashimaya.png", //21
+    "image/takashimaya.png", //20
+    "image/takashimaya.png", //19
+    "image/takashimaya.png", //18
+    "image/takashimaya.png", //17
+    "image/takashimaya.png", //16
+    "image/takashimaya.png", //15
+    "image/takashimaya.png", //14
+    "image/xiaomi.png", //13
+    "image/xiaomi.png", //12
+    "image/xiaomi.png", //11
+    "image/xiaomi.png", //10
+    "image/xiaomi.png", //9
+    "image/xiaomi.png", //8
+    "image/xiaomi.png", //7
+    "image/xiaomi.png", //6
+    "image/xiaomi.png", //5
+    "image/xiaomi.png", //4
+    "image/nespresso.png", // 3
+    "image/dji.png", // 2
+    "image/dyson.png", // 1
 ];
 
 const allPrizeText = [
-    "KrisPay Miles 10,200",
-    "KrisPay Miles 10,200",
-    "#27 Cornell Kettle 1.7L ",
-    "#26 Cornell Kettle 1.7L",
-    "#25 Bodum French Press Coffee Maker",
-    "#24 Kris Pay Miles 7,500",
-    "#23 Kris Pay Miles 7,500",
-    "#22 Kris Pay Miles 7,500",
-    "#21 Kris Pay Miles 7,500",
-    "#20 Eu Yan Seng: Birds' Nest (assorted)",
-    "#19 Aeropress Coffee Maker",
-    "#18 Grand Copthorne Weekday Dinner Buffet for 2",
-    "#17 JBL CLIP3 Waterproof Speaker",
-    "#16 Nestle IONA 12inch Stand Fan",
-    "#15 Happycall 28cm Die Cast Party Wok + Steamer",
-    "#14 Happycall 28cm Die Cast Party Wok + Steamer",
-    "#13 Apple Airtag (4 pieces)",
-    "#12 Jo Malone Candle",
-    "#11 Jabra Elite 4 Active (Navy)",
-    "#10 Sony WH-CH720N Headphones (Black)",
-    "#9 Happycall 26cm Deep Wok Pan + Lid",
-    "#8 Dior Prestige Le Micro Serum (Advanced)",
-    "#7 Sterra Sun Dehumidifier",
-    "#6 Nintendo Switch (OLED)",
-    "#5 iPad<br>(Sponsored by FPT)",
-    "#4 Dior Prestige Set",
-    "#3 Insta360 X4 8K Action Camera<br>(Sponsored by Amadeus)",
-    "#2 Dyson V12 Origin<br>(Sponsored by Checkpoint)",
-    "#1 Bang & Olufsen Beoplay HX<br>(Sponsored by TCS)"
+    "#23 $20 Takashimaya Gift Voucher",
+    "#22 $20 Takashimaya Gift Voucher",
+    "#21 $20 Takashimaya Gift Voucher",
+    "#20 $20 Takashimaya Gift Voucher",
+    "#19 $20 Takashimaya Gift Voucher",
+    "#18 $20 Takashimaya Gift Voucher",
+    "#17 $20 Takashimaya Gift Voucher",
+    "#16 $20 Takashimaya Gift Voucher",
+    "#15 $20 Takashimaya Gift Voucher",
+    "#14 $20 Takashimaya Gift Voucher",
+    "#13 $20 Takashimaya Gift Voucher",
+    "#12 Xiaomi Mi Wireless Power Bank (10000mAh)",
+    "#11 Xiaomi Mi Wireless Power Bank (10000mAh)",
+    "#10 Xiaomi Mi Wireless Power Bank (10000mAh)",
+    "#9 Xiaomi Mi Wireless Power Bank (10000mAh)",
+    "#8 Xiaomi Mi Wireless Power Bank (10000mAh)",
+    "#7 Xiaomi Mi Wireless Power Bank (10000mAh)",
+    "#6 Xiaomi Mi Wireless Power Bank (10000mAh)",
+    "#5 Xiaomi Mi Wireless Power Bank (10000mAh)",
+    "#4 Xiaomi Mi Wireless Power Bank (10000mAh)",
+    "#3 Nespresso Essenza Mini Pure White",
+    "#2 DJI Osmo Action 4 Standard",
+    "#1 Dyson Supersonic™ Hair Dryer"
 ];
 
 showSlides();
 
 function showSlides() {
     let slidesLeft = document.querySelectorAll('.slide');
-    let slidesRight = document.querySelectorAll('.slide');
-
-    //slidesLeft.forEach(slide => slide.style.display = 'none');
-    //slidesRight.forEach(slide => slide.style.display = 'none');
 
     slideIndex++;
 
     if (slideIndex > totalSlides) {
         slideIndex = 1;
     }
-    // Check if the slideIndex exceeds the total number of slides on either side
-
-
-    // Alternate between left and right slides
-    // if (slideIndex % 2 === 0) {
     slidesLeft[0].style.display = 'block';
-    //slidesRight[0].style.display = 'block';
     slidesLeft[0].src = allImages[slideIndex - 1];
-    // } else {
-    //     //slidesLeft[0].style.display = 'block';
-    //     slidesRight[0].style.display = 'block';
-    //     slidesRight[0].src = allImages[slideIndex - 1];
+}
+
+
+const names = [];
+const winners = [
+    // {
+    //     "item": "",
+    //     "name": ""
     // }
+]; // load from localstorage immediately
 
-    //setTimeout(showSlides, 5000); // Change image every 5 seconds
+console.log(localStorage.getItem('participants'));
+console.log(localStorage.getItem('winners'));
+
+function readSingleFile(evt) {
+    var f = evt.target.files[0];
+    if (f) {
+        var r = new FileReader();
+        r.onload = function (e) {
+            var contents = e.target.result;
+
+            var lines = contents.split("\n");
+            for (var i = 0; i < lines.length; i++) {
+                if (lines[i].split(",").join("").trim() !== "") {
+                    names.push(lines[i].split(",").join("").trim());
+                }
+            }
+            updateNameList();
+            localStorage.setItem('participants', JSON.stringify(names));
+            localStorage.setItem('winners', JSON.stringify(winners));
+        }
+        r.readAsText(f);
+        // console.log(localStorage.getItem('participants'));
+    } else {
+        alert("Failed to load file");
+    }
 }
 
-// function showSlides() {
-//     let slidesLeft = document.querySelectorAll('.slideshow-left .slide');
-//     let slidesRight = document.querySelectorAll('.slideshow-right .slide');
-
-//     slidesLeft.forEach(slide => slide.style.display = 'none');
-//     slidesRight.forEach(slide => slide.style.display = 'none');
-
-//     slideIndex++;
-//     if (slideIndex > slidesLeft.length) { slideIndex = 1; }
-
-//     slidesLeft[slideIndex - 1].style.display = 'block';
-//     slidesRight[slideIndex - 1].style.display = 'block';
-
-//     setTimeout(showSlides, 5000); // Change image every 5 seconds
-// }
-
-let names = [];
-
-const firstNames = ["John", "Jane", "Alex", "Emily", "Chris", "Katie", "Michael", "Sarah", "David", "Laura"];
-const lastNames = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Martinez", "Hernandez"];
-
-function getRandomName() {
-    const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
-    const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
-    return `${firstName} ${lastName}`;
+function updateNameList() {
+    const nameList = document.getElementById('nameList');
+    nameList.innerHTML = '<h3>Participants:</h3>' + names.map(name => `<p>${name}</p>`).join('');
 }
 
-function deleteName() {
-    const nameInput = document.getElementById('nameInput');
+function deleteName(nameInput) { // used to remove winner after drawing
+    // const nameInput = document.getElementById('nameInput');
     const name = nameInput.value.trim(); // Get the input value and trim any extra spaces
 
     if (name) {
         // Find the index of the name in the names array
         const index = names.indexOf(name);
-
         if (index > -1) { // If the name is found
+            winners.push({ item: allPrizeText[slideIndex], name: names[index] });
+            localStorage.setItem('winners', JSON.stringify(winners));
 
-            let newarr = names.filter(a => a !== name)
-            names = newarr;
-            //names.splice(index, 1); // Remove the name from the array
-            //nameInput.value = ''; // Clear the input field
-            //alert(names);
+            names.splice(index, 1);
             updateNameList(); // Update the displayed name list
-            alert("!!! Winner removed !!!");
+            localStorage.setItem('participants', JSON.stringify(names))
             document.getElementById('nameInput').value = "";
         } else {
             alert('Name not found in the list!');
@@ -147,34 +129,16 @@ function deleteName() {
     } else {
         alert('Please enter a name to delete!');
     }
+    console.log(localStorage.getItem('participants'));
+    console.log(localStorage.getItem('winners'));
 }
 
-function addName() {
-    const nameInput = document.getElementById('nameInput');
-    const name = nameInput.value.trim();
-    if (name) {
-        names.push(name);
-        nameInput.value = '';
-        updateNameList();
-    }
-}
 
-function addRandomNames() {
-    for (let i = 0; i < 500; i++) {
-        names.push(getRandomName());
-    }
-    updateNameList();
-}
-
-function updateNameList() {
-    const nameList = document.getElementById('nameList');
-    nameList.innerHTML = '<h3>Participants:</h3>' + names.map(name => `<p>${name}</p>`).join('');
-}
 var play_effect;
 var win_effect;
 let prevPosition = 0;
-function drawWinner() {
-    confetti.stop()
+function drawWinner() { // index.html
+    confetti.stop() // confetti.js
     stop();
     if (play_effect) {
         play_effect.pause()
@@ -229,32 +193,6 @@ function drawWinner() {
     }, 100);
 }
 
-function readSingleFile(evt) {
-    names = [];
-    var f = evt.target.files[0];
-    if (f) {
-        var r = new FileReader();
-        r.onload = function (e) {
-            var contents = e.target.result;
-            //document.write("File Uploaded! <br />" + "name: " + f.name + "<br />" + "content: " + contents + "<br />" + "type: " + f.type + "<br />" + "size: " + f.size + " bytes <br />");
-
-            var lines = contents.split("\n"), output = [];
-            for (var i = 0; i < lines.length; i++) {
-                names.push(lines[i].split(",").join("").trim());
-                updateNameList();
-                //output.push("<tr><td>" + lines[i].split(",").join("</td><td>") + "</td></tr>");
-            }
-            //output = "<table>" + output.join("") + "</table>";
-            //document.write(output);
-        }
-        r.readAsText(f);
-        //document.write(output);
-    } else {
-        alert("Failed to load file");
-    }
-
-}
-
 // Function to create and append confetti elements
 function createConfetti() {
     const confettiContainer = document.getElementById('confetti');
@@ -301,23 +239,20 @@ const stop = () => {
 
 document.getElementById('fileinput').addEventListener('change', readSingleFile);
 
-function previous() {
+function previous() { // prev prize button
     slideIndex--;
     if (slideIndex < 1) {
-        slideIndex = 29;
+        slideIndex = totalSlides;
     }
-
     document.getElementById('slidesLeft').src = allImages[slideIndex - 1];
     document.getElementById('prize_desc').innerHTML = allPrizeText[slideIndex - 1];
 }
 
-function next() {
-
+function next() { // next prize button
     slideIndex++;
     if (slideIndex > totalSlides) {
         slideIndex = 1;
     }
-
     document.getElementById('slidesLeft').src = allImages[slideIndex - 1];
     document.getElementById('prize_desc').innerHTML = allPrizeText[slideIndex - 1];
 
